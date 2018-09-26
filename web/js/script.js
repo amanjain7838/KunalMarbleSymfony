f044e2b5f9117d095b22312d710c0b43c5f776b9
Aman Jain <amanjain7838@gmail.com>
2018-09-27 01:15:23 +0530

    Second

---
+++ web/js/script.js
@@ -0,0 +1,276 @@
+/**
+ * @function      Include
+ * @description   Includes an external scripts to the page
+ * @param         {string} scriptUrl
+ */
+function include(scriptUrl) {
+    document.write('<script src="' + scriptUrl + '"></script>');
+}
+
+/**
+ * @function      isIE
+ * @description   checks if browser is an IE
+ * @returns       {number} IE Version
+ */
+function isIE() {
+    var myNav = navigator.userAgent.toLowerCase();
+    return (myNav.indexOf('msie') != -1) ? parseInt(myNav.split('msie')[1]) : false;
+};
+
+
+/* Easing library
+ ========================================================*/
+include('js/jquery.easing.1.3.js');
+
+/* PointerEvents
+ ========================================================*/
+;
+(function ($) {
+    if(isIE() && isIE() < 11){
+        include('js/pointer-events.js');
+        $('html').addClass('lt-ie11');
+        $(document).ready(function(){
+            PointerEventsPolyfill.initialize({});
+        });
+    }
+})(jQuery);
+
+/* Stick up menus
+ ========================================================*/
+;
+(function ($) {
+    var o = $('html');
+    if (o.hasClass('desktop')) {
+        include('js/tmstickup.js');
+
+        $(document).ready(function () {
+            $('#stuck_container').TMStickUp({})
+        });
+    }
+})(jQuery);
+
+/**
+ * @module       ToTop
+ * @description  Enables ToTop Plugin
+ */
+;
+(function ($) {
+    var o = $('html');
+    if (o.hasClass('desktop')) {
+        include('js/jquery.ui.totop.js');
+
+        $(document).ready(function () {
+            $().UItoTop({
+                easingType: 'easeOutQuart',
+                containerClass: 'toTop  fl-free-chaos-up153 '
+            });
+        });
+    }
+})(jQuery);
+
+/* EqualHeights
+ ========================================================*/
+;
+(function ($) {
+    var o = $('[data-equal-group]');
+    if (o.length > 0) {
+        include('js/jquery.equalheights.js');
+    }
+})(jQuery); 
+
+/**
+ * @module       Copyright
+ * @description  Evaluates the copyright year
+ */
+;
+(function ($) {
+    var currentYear = (new Date).getFullYear();
+    $(document).ready(function () {
+        $("#copyright-year").text((new Date).getFullYear());
+    });
+})(jQuery);
+
+
+
+
+/* Camera
+ ========================================================*/
+;(function ($) {
+    var o = $('#camera');
+    if (o.length > 0) {
+        if (!(isIE() && (isIE() > 9))) {
+            include('js/jquery.mobile.customized.min.js');
+        }
+
+        include('js/camera.js');
+
+        $(document).ready(function () {
+            o.camera({
+                autoAdvance: true,
+                height: '39.51219512195122%',
+                minHeight: '810px',
+                pagination: false,
+                thumbnails: false,
+                playPause: false,
+                hover: false,
+                loader: 'none',
+                navigation: true,
+                navigationHover: false,
+                mobileNavHover: false,
+                fx: 'simpleFade'
+            })
+        });
+    }
+})(jQuery);
+
+/**
+ * @module       RD Google Map
+ * @description  Enables RD Google Map Plugin
+ */
+;
+(function ($) {
+    var o = document.getElementById("google-map");
+    if (o) {
+        include('//maps.google.com/maps/api/js?sensor=false');
+        include('js/jquery.rd-google-map.js');
+
+        $(document).ready(function () {
+            var o = $('#google-map');
+            if (o.length > 0) {
+                o.googleMap();
+            }
+        });
+    }
+})
+(jQuery);
+
+/**
+ * @module       WOW Animation
+ * @description  Enables scroll animation on the page
+ */
+;
+(function ($) {
+    var o = $('html');
+
+    if ((navigator.userAgent.toLowerCase().indexOf('msie') == -1 ) || (isIE() && isIE() > 9)) {
+        if (o.hasClass('desktop')) {
+            include('js/wow.js');
+
+            $(document).ready(function () {
+                new WOW().init();
+            });
+        }
+    }
+})(jQuery);
+
+
+/* TouchTouch Gallery
+ ========================================================*/
+;(function ($) {
+    var o = $('.thumb');
+    if (o.length > 0) {
+        include('js/jquery.touch-touch.js');
+        $(document).ready(function () {
+            o.touchTouch();
+        });
+    }
+})(jQuery);
+
+/* Mailform
+=============================================*/
+;(function ($) {
+    var o = $('.rd-mailform');
+    if (o.length > 0) {
+        include('js/mailform/jquery.form.min.js'); 
+        include('js/mailform/jquery.rd-mailform.min.js');
+
+        $(document).ready(function () {
+            var o = $('.rd-mailform');
+
+            if (o.length) {
+                o.rdMailForm({
+                    validator: {
+                        'constraints': {
+                            '@LettersOnly': {
+                                message: 'Please use letters only!'
+                            },
+                            '@NumbersOnly': {
+                                message: 'Please use numbers only!'
+                            },
+                            '@NotEmpty': {
+                                message: 'Field should not be empty!'
+                            },
+                            '@Email': {
+                                message: 'Enter valid e-mail address!'
+                            },
+                            '@Phone': {
+                                message: 'Enter valid phone number!'
+                            },
+                            '@Date': {
+                                message: 'Use MM/DD/YYYY format!'
+                            },
+                            '@SelectRequired': {
+                                message: 'Please choose an option!'
+                            }
+                        }
+                    }
+                }, {
+                    'MF000': 'Sent',
+                    'MF001': 'Recipients are not set!',
+                    'MF002': 'Form will not work locally!',
+                    'MF003': 'Please, define email field in your form!',
+                    'MF004': 'Please, define type of your form!',
+                    'MF254': 'Something went wrong with PHPMailer!',
+                    'MF255': 'There was an error submitting the form!'
+                });
+            }
+        });
+    }
+})(jQuery);
+/* Orientation tablet fix
+ ========================================================*/
+$(function () {
+    // IPad/IPhone
+    var viewportmeta = document.querySelector && document.querySelector('meta[name="viewport"]'),
+        ua = navigator.userAgent,
+
+        gestureStart = function () {
+            viewportmeta.content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6, initial-scale=1.0";
+        },
+
+        scaleFix = function () {
+            if (viewportmeta && /iPhone|iPad/.test(ua) && !/Opera Mini/.test(ua)) {
+                viewportmeta.content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
+                document.addEventListener("gesturestart", gestureStart, false);
+            }
+        };
+
+    scaleFix();
+    // Menu Android
+    if (window.orientation != undefined) {
+        var regM = /ipod|ipad|iphone/gi,
+            result = ua.match(regM);
+        if (!result) {
+            $('.sf-menus li').each(function () {
+                if ($(">ul", this)[0]) {
+                    $(">a", this).toggle(
+                        function () {
+                            return false;
+                        },
+                        function () {
+                            window.location.href = $(this).attr("href");
+                        }
+                    );
+                }
+            })
+        }
+    }
+});
+var ua = navigator.userAgent.toLocaleLowerCase(),
+    regV = /ipod|ipad|iphone/gi,
+    result = ua.match(regV),
+    userScale = "";
+if (!result) {
+    userScale = ",user-scalable=0"
+}
+document.write('<meta name="viewport" content="width=device-width,initial-scale=1.0' + userScale + '">');
